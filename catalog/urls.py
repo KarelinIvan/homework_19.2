59533887b47642a17ada3a1e0c168261ddbcfbb2
i.karelin <ivan,karelin.1993@mail.ru>
2024-09-06 10:11:16 +0300

    Седьмой коммит

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,5 @@
 from xml.etree.ElementInclude import include
 
-from django.contrib import admin
-from django.urls import path
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog'))
